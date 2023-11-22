857bb598e766cbda6a1f82e22de5e5e7a259eaca
Null <pairmanxlr@gmail.com>
2023-11-22 17:02:54 +0800

    Update chaoxing.py

--- utils/chaoxing.py
+++ utils/chaoxing.py
@@ -10,18 +10,44 @@ session = requests.session()
 
 locations = {
 	"A": "",
-	'B': "{\"result\":1,\"latitude\":34.132297,\"longitude\":108.838367,\"address\":\"中国陕西省西安市长安区兴隆街道内环北路西安电子科技大学(南校区)\"}",
+	"B": "{\"result\":1,\"latitude\":34.132297,\"longitude\":108.838367,\""\
+	     "address\":\"中国陕西省西安市长安区兴隆街道内环北路西安电子科技大学(南"\
+	     "校区)\"}",
 	"C": "",
 	"信远": "",
 }
 
-def chaoxing_login(username: str, password: str, verify: int = 0):
+def chaoxing_headers() -> dict[str, str]:
+	"""Get request headers.
+	:return: Request headers.
+	"""
+	ua = "Mozilla/5.0 (iPhone; CPU iPhone OS 16_7_2 like Mac OS X) AppleWe"\
+	     "bKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 com.ssreader.Cha"\
+	     "oXingStudy/ChaoXingStudy_3_6.2.3_ios_phone_202311102230_219 (@Ka"\
+	     "limdor)_9048867483954592879"
+	data = requests.utils.dict_from_cookiejar(session.cookies)
+	cookie = "".join((key + "=" + val + ";" for key, val in data.items()))
+	headers = {
+		"Accept-Encoding": "gzip, deflate, br",
+	    	"Accept-Language": "en-US,en;q=0.9",
+		"Cookie": cookie, 
+		"User-Agent": ua,
+		"sec-ch-ua-mobile": "?1",
+		"sec-ch-ua-platform": "iOS",
+		"Sec-Fetch-Dest": "empty",
+		"Sec-Fetch-Mode": "cors",
+		"Sec-Fetch-Site": "same-origin",
+		"X-Requested-With": "XMLHttpRequest"
+	}
+	return headers
+
+def chaoxing_login(username: str, password: str) -> dict[str, str]:
 	"""Login to Chaoxing account.
 	:param username: Username.
 	:param password: Password.
-	:param verify: SSL certificate verification toggle.
-	:return: Cookie, name, FID and UID in dictionary.
+	:return: Name, FID, TID and UID on success, otherwise empty dict.
 	"""
+	url = "https://passport2-api.chaoxing.com/v11/loginregister"
 	params = {
 		"code": password,
 		"cx_xxt_passport": "json",
@@ -29,73 +55,176 @@ def chaoxing_login(username: str, password: str, verify: int = 0):
 		"roleSelect": "true",
 		"uname": username
 	}
-	url = "https://passport2-api.chaoxing.com/v11/loginregister"
-	res = session.get(url, params = params, verify = verify)
-	data = requests.utils.dict_from_cookiejar(session.cookies).items()
-	cookie = "".join((key + "=" + val + ";" for key, val in data))
+	res = session.get(url, params = params, verify = 0)
 	d = loads(res.text)
 	if d["mes"] == "验证通过":
 		url = "https://sso.chaoxing.com/apis/login/userLogin4Uname.do"
-		res = session.get(url, verify = verify)
+		res = session.get(url, verify = 0)
 		d = loads(res.text)
 		if d["result"] == 1:
-			name, fid, uid = str(d["msg"]["name"]), str(d["msg"]["fid"]), str(d["msg"]["puid"])
-			return {"cookie": cookie, "name": name, "fid": fid, "uid": uid}
-	return {"cookie": "", "name": "", "fid": fid, "uid": ""}
+			data = requests.utils.dict_from_cookiejar(session.
+					     			  cookies)
+			return {
+				"name": str(d["msg"]["name"]),
+				"fid": data["fid"],
+				"tid": data["_tid"],
+				"uid": data["_uid"]
+			}
+	return {}
 
-def chaoxing_headers(cookie: str):
-	"""Get chaoxing request headers.
-	:param cookie: Cookie.
-	:return: Headers.
-	"""
-	ua = "Mozilla/5.0 (iPhone; CPU iPhone OS 16_7_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 com.ssreader.ChaoXingStudy/ChaoXingStudy_3_6.2.3_ios_phone_202311102230_219 (@Kalimdor)_9048867483954592879"
-	headers = {
-		"Accept-Encoding": "gzip",
-	    	"Accept-Language": "en-US,en;q=0.9",
-		"User-Agent": ua,
-		"Cookie": cookie
-	}
-	return headers
-
-def chaoxing_login_check(cookie: str, verify: int = 0):
-	"""Check chaoxing account login state.
-	:param cookie: Cookie.
-	:param verify: SSL certificate verification toggle.
-	:return: Returns 1 if login is valid, otherwise 0.
+def chaoxing_login_check() -> bool:
+	"""Unused. Check login state.
+	:return: Returns True on valid login, otherwise False.
 	"""
 	url = "https://sso.chaoxing.com/apis/login/userLogin4Uname.do"
-	res = requests.get(url, headers = chaoxing_headers(cookie), verify = verify)
+	res = session.get(url, headers = chaoxing_headers(), verify = 0)
 	d = loads(res.text)
 	return d["result"] == 1
 
-def chaoxing_checkin_url_checkurl(checkin_url: str):
-	"""Check if URL is valid qrcode-Location checkin URL.
-	:return: Returns 1 if valid, otherwise 0.
+def chaoxing_checkin_details(active_id: str) -> dict[str, str]:
+	"""Get checkin details via checkin activity ID.
+	:param active_id: Activity ID.
+	:param msg: MSG code.
+	:return: Checkin details including class ID and MSG code.
 	"""
-	return not checkin_url.find("https://mobilelearn.chaoxing.com/widget/sign/e")
+	url = "https://mobilelearn.chaoxing.com/newsign/signDetail"
+	params = {
+		"activePrimaryId": active_id,
+		"type": "1"
+	}
+	res = session.get(url, params = params, verify = 0)
+	return loads(res.text)
 
-def chaoxing_checkin_url(checkin_url: str, userinfo: dict[str, str], location: str, verify: int = 0):
-	"""Qrcode-location checkin.
-	:param checkin_url: URL from checkin qrcode.
-	:param userinfo: Cookie, name, FID and UID in dictionary.
-	:param location: Location.
-	:param verify: SSL certificate verification toggle.
-	:return: Returns 1 if checkin success, otherwise 0.
+def chaoxing_checkin_presign(active_id: str, userinfo: dict[str, str],
+			     sign_details: dict[str, str] = {}) -> bool:
+	"""Checkin pre-sign.
+	:param active_id: Activity ID.
+	:param userinfo: TID and UID. 
+	:return: Returns True on success, otherwise False.
 	"""
-	params_l = parse_qs(urlparse(unquote(checkin_url)).query)
-	name, uid, fid, cookie = userinfo["name"], userinfo["uid"], userinfo["fid"], userinfo["cookie"]
-	active_id, enc = params_l["id"][0], params_l["enc"][0]
+	if not sign_details:
+		sign_details = chaoxing_checkin_details(active_id = active_id)
+	url = "https://mobilelearn.chaoxing.com/newsign/preSign"
+	params = {
+		"courseId": "",
+		"classId": sign_details["clazzId"],
+		"activePrimaryId": active_id,
+		"general": "1",
+		"sys": "1",
+		"ls": "1",
+		"appType": "15",
+		"tid": userinfo["tid"],
+		"uid": userinfo["uid"],
+		"ut": "s"
+	}
+	res = session.get(url, params = params, headers = chaoxing_headers(),
+			  verify = 0)
+	return res.status_code == 200
+
+def chaoxing_checkin_analysis(active_id: str) -> bool:
+	"""Checkin analysis.
+	:param active_id: Activity ID. 
+	:return: Returns True on success, otherwise False.
+	"""
+	url = "https://mobilelearn.chaoxing.com/pptSign/analysis"
+	params = {
+		"vs": "1",
+		"DB_STRATEGY": "RANDOM",
+		"aid": active_id
+	}
+	res = session.get(url, params = params, headers = chaoxing_headers(),
+			  verify = 0)
+	code = res.text.split("'")[5]
+	url = "https://mobilelearn.chaoxing.com/pptSign/analysis2"
+	params = {
+		"DB_STRATEGY": "RANDOM",
+		"code": code
+	}
+	res = session.get(url, params = params, headers = chaoxing_headers(),
+		   	  verify = 0)
+	return res.text == "success"
+
+def chaoxing_checkin_qrloc_checkin(active_id: str, enc: str,
+				   userinfo: dict[str, str],
+				   location: str) -> bool:
+	"""Qrcode-location checkin.
+	:param active_id: Activity ID.
+	:param enc: ENC code.
+	:param userinfo: Name, FID and UID.
+	:param location: Location.
+	:return: Returns True on success, otherwise False.
+	"""
+	sign_details = chaoxing_checkin_details(active_id)
+	if not chaoxing_checkin_presign(active_id = active_id,
+				 	userinfo = userinfo,
+					sign_details = sign_details):
+		return False
+	if not chaoxing_checkin_analysis(active_id = active_id):
+		return False
 	url = "https://mobilelearn.chaoxing.com/pptSign/stuSignajax"
 	params = {
-		'enc': enc,
-		'name': name,
-		'activeId': active_id,
-		'uid': uid,
-		'location': location,
-		'latitude': '-1',
-		'longitude': '-1',
-		'fid': fid,
-		'appType': '15'
+		"enc": enc,
+		"name": userinfo["name"],
+		"activeId": active_id,
+		"uid": userinfo["uid"],
+		"clientip": "",
+		"location": location,
+		"latitude": sign_details["latitude"],
+		"longitude": sign_details["longitude"],
+		"fid": userinfo["fid"],
+		"appType": "15"
 	}
-	res = requests.get(url, params = params, headers = chaoxing_headers(cookie), verify = verify)
+	res = session.get(url, params = params, headers = chaoxing_headers(),
+		   	  verify = 0)
+	return res.text == "success"
+
+def chaoxing_checkin_qrloc_checkin_url(checkin_url: str,
+				       userinfo: dict[str, str],
+				       location: str) -> bool:
+	"""Qrcode-location checkin via URL.
+	:param checkin_url: URL from checkin qrcode.
+	:param userinfo: Name, FID and UID.
+	:param location: Location.
+	:return: Returns True on success, otherwise False.
+	"""
+	if checkin_url.find("https://mobilelearn.chaoxing.com/widget/sign/e"):
+		return False
+	params = parse_qs(urlparse(unquote(checkin_url)).query)
+	return chaoxing_checkin_qrloc_checkin(active_id = params["id"][0],
+					      enc = params["enc"][0],
+					      userinfo = userinfo,
+					      location = location)
+
+def chaoxing_checkin_loc_checkin(active_id: str, userinfo: dict[str, str],
+				 location: str) -> bool:
+	"""Location checkin.
+	:param active_id: Activity ID.
+	:param userinfo: Name, FID and UID.
+	:param location: Location.
+	:return: Returns True on success, otherwise False.
+	"""
+	sign_details = chaoxing_checkin_details(active_id)
+	if not chaoxing_checkin_presign(active_id = active_id,
+				 	userinfo = userinfo,
+					sign_details = sign_details):
+		return False
+	if not chaoxing_checkin_analysis(active_id = active_id):
+		return False
+	url = "https://mobilelearn.chaoxing.com/pptSign/stuSignajax"
+	params = {
+		"name": userinfo["name"],
+		"address": loads(location)["address"],
+		"activeId": active_id,
+		"uid": userinfo["uid"],
+		"clientip": "",
+		"latitude": loads(location)["latitude"],
+		"longitude": loads(location)["longitude"],
+		"fid": userinfo["fid"],
+		"appType": "15",
+		"ifTiJiao": str(int(sign_details["latitude"] ==
+		      		    sign_details["longitude"] == 0)),
+		"validate": ""
+	}
+	res = session.get(url, params = params, headers = chaoxing_headers(),
+		   	  verify = 0)
 	return res.text == "success"
