c1e7f9a5fc0bfb8f97df6b35474a7c57342f27af
Null <pairmanxlr@gmail.com>
2023-11-23 21:32:10 +0800

    Update xdcheckin.py

--- xdcheckin.py
+++ xdcheckin.py
@@ -1,47 +1,61 @@
 from cv2 import VideoCapture, imshow, waitKey, destroyAllWindows, QRCodeDetector
 from json import load
-from utils.chaoxing import *
+from chaoxing import *
 
-def scan_checkin():
-	"""QR-Location checkin via camera scan.
+def loc(username, password):
+	"""Location checkin interface.
+	Input activity IDs to checkin.
 	"""
+	cx = Chaoxing(username, password)
+	if (not cx.logined):
+		exit("[xdcheckin] (login) failed.")
+	print("[xdcheckin] (login) successful.")
+	while active_id := input():
+		try:
+			checkin = cx.checkin_checkin_location({"active_id": active_id}, locations["B"])
+		except Exception:
+			checkin = False
+		print("[xdcheckin] (location_checkin) " + ("successful." if checkin else "failed."))
+
+def qr(username, password):
+	"""Qrcode checkin interface.
+	Scan qrcodes to checkin.
+	"""
+	cx = Chaoxing(username, password)
+	if (not cx.logined):
+		exit("[xdcheckin] (login) failed.")
+	print("[xdcheckin] (login) successful.")
 	video = VideoCapture(0)
 	qcd = QRCodeDetector()
 	decoded_data_set_old = set()
 	while True:
 		frame = video.read()[1]
 		decoded_data_set = set(qcd.detectAndDecodeMulti(frame)[1])
-		if len(decoded_data_set) and decoded_data_set != decoded_data_set_old:
-				print(decoded_data_set_old := decoded_data_set)
+		if len(decoded_data_set) and tuple(decoded_data_set)[0] != "" and decoded_data_set != decoded_data_set_old:
+				decoded_data_set_old = decoded_data_set
 				for url in decoded_data_set:
-					success = 0
 					try:
-						success = chaoxing_checkin_qrloc_checkin_url(url, config, locations["B"])
+						checkin = cx.checkin_checkin_qrcode_url(url, locations["B"])
 					except Exception:
-						print(Exception)
-					print("[xdcheckin] (scan_checkin) %s" % "checkin success" if success else "checkin failed")
+						checkin = False
+					print("[xdcheckin] (qrcode_checkin) " + ("successful." if checkin else "failed."))
 		imshow("frame", frame)
 		if waitKey(1) & 0xFF == ord("q"):
 			break
 	video.release()
 	destroyAllWindows()
 
-def loc_checkin():
-	"""Location checkin from user input.
-	"""
-	while active_id := input():
-		try:
-			success = chaoxing_checkin_loc_checkin(active_id = active_id, userinfo = userinfo, location = locations["B"])
-		except Exception:
-			print(Exception)
-		print("[xdcheckin] (loc_checkin) %s" % "checkin success" if success else "checkin failed")
-
 if __name__ == "__main__":
-	# Login.
-	with open("config.json", "r", encoding = "utf-8") as config_file:
-		config = load(config_file)
-		if not (userinfo := chaoxing_login(config["username"], config["password"])):
-			exit("[xdcheckin] (login) failed.")
-		print("[xdcheckin] (login) success.")
-	# Checkin.
-	loc_checkin()
+	username = password = ""
+	try:
+		with open("config.json", "r", encoding = "utf-8") as config_file:
+			config = load(config_file)
+			print(config)
+		assert (username := config["username"])
+		assert (password := config["password"])
+	except Exception:
+		exit("[xdcheckin] (main) error loading config.")
+	try:
+		qr(username = username, password = password)
+	except Exception:
+		exit("[xdcheckin] (main) error checking in.")
