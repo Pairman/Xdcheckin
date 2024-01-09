62193f95dd68905707a04577f7fc5bfc69930dcb
Null <pairmanxlr@gmail.com>
2024-01-10 03:32:32 +0800

    revert toga style oack removal

--- src/xdcheckin/app.py
+++ src/xdcheckin/app.py
@@ -1,5 +1,6 @@
 from threading import Thread
 from toga import App as toga_App, Box as toga_Box, MainWindow as toga_MainWindow, WebView as toga_WebView
+from toga.style import Pack as toga_style_Pack
 from waitress import serve
 from backend.server import server
 
@@ -10,7 +11,7 @@ class Xdcheckin(toga_App):
 		self.main_window = toga_MainWindow(title = self.formal_name)
 		self.main_window.content = toga_Box(
 			children = [
-				toga_WebView(url = "http://127.0.0.1:5001")
+				toga_WebView(url = "http://127.0.0.1:5001", style = toga_style_Pack(flex = 1))
 			]
 		)
 		self.main_window.show()
