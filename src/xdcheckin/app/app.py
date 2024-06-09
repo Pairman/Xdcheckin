ca644f8ea660cf71441ca8a4a054b420041c9858
Null <pairmanxlr@gmail.com>
2024-06-10 02:13:23 +0800

    Update app.py

--- src/xdcheckin/app/app.py
+++ src/xdcheckin/app/app.py
@@ -1,6 +1,6 @@
 __all__ = ()
 
-from multiprocessing import Process as _Process
+from threading import Thread as _Thread
 from toga import App as _App, Box as _Box, MainWindow as _MainWindow, \
 WebView as _WebView, Label as _Label
 from toga.platform import current_platform as _current_platform
@@ -12,8 +12,7 @@ _msg = f"This APP will launch\n your browser automatically.\n\
 If not, visit \"{_url}\"\n in you browser manually."
 
 class _Xdcheckin(_App):
-	def startup(self):
-		_Process(target = _start_server, daemon = True).start()
+	def _gui_wrapper(self):
 		self.main_window = _MainWindow(title = self.formal_name)
 		if _current_platform == "android":
 			from android.content import Intent
@@ -37,3 +36,7 @@ class _Xdcheckin(_App):
 				url = _url, style = _Pack(flex = 1)
 			), ))
 		self.main_window.show()
+
+	def startup(self):
+		_Thread(target = self._gui_wrapper, daemon = True).start()
+		_start_server()
