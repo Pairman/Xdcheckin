2728417cc1acac15efced279d440863213ce0289
Null <pairmanxlr@gmail.com>
2024-08-25 00:25:56 +0800

    Update activity.js

--- src/xdcheckin/server/static/activity.js
+++ src/xdcheckin/server/static/activity.js
@@ -2,27 +2,27 @@ async function getActivities() {
 	if (getActivities.calling)
 		return;
 	getActivities.calling = true;
-	let e = document.getElementById("activities-list-div");
+	const e = document.getElementById("activities-list-div");
 	e.replaceChildren();
-	let activities = (await post("/chaoxing/get_activities")).json();
+	const activities = (await post("/chaoxing/get_activities")).json();
 	if (!Object.keys(activities).length)
 		e.innerText = "No ongoing activities.";
 	for (let class_id in activities) {
-		let course_activities = activities[class_id];
+		const course_activities = activities[class_id];
 		e.appendChild(newElement("div", {
 			innerText: `${g_courses[class_id].name}: `
 		}));
 		course_activities.forEach(a => {
-			let type = a.type == "2" ? `Qrcode` : `Location`;
-			let ts = a.time_start;
-			let te = a.time_end || "????-??-?? ??:??";
-			let ts_y = ts.slice(0, 4);
-			let ts_md = ts.slice(5, 10);
-			let ts_hm = ts.slice(11, 16);
-			let te_y = te.slice(0, 4);
-			let te_md = te.slice(5, 10);
-			let te_hm = te.slice(11, 16);
-			ts = "", te = "";
+			const type = a.type == "2" ? `Qrcode` : `Location`;
+			const ts_a = a.time_start;
+			const te_a = a.time_end || "????-??-?? ??:??";
+			const ts_y = ts_a.slice(0, 4);
+			const ts_md = ts_a.slice(5, 10);
+			const ts_hm = ts_a.slice(11, 16);
+			const te_y = te_a.slice(0, 4);
+			const te_md = te_a.slice(5, 10);
+			const te_hm = te_a.slice(11, 16);
+			let ts = "", te = "";
 			if (ts_y != te_y) {
 				ts += `${ts_y}-`;
 				te += `${te_y}-`;
@@ -33,7 +33,7 @@ async function getActivities() {
 			}
 			ts += ts_hm;
 			te += te_hm;
-			let b = newElement("button", {
+			const b = newElement("button", {
 				id: `chaoxing-activity-${a.active_id}-button`,
 				disabled: a.type == "2",
 				innerText: `${a.name} (${type}, ${ts} ~ ${te})`,
@@ -46,7 +46,7 @@ async function getActivities() {
 }
 
 async function chaoxingCheckinCaptcha(params, captcha, e_id_prefix) {
-	let res = await post("/chaoxing/captcha_get_captcha", data = {
+	const res = await post("/chaoxing/captcha_get_captcha", data = {
 		"captcha": captcha
 	})
 	if (res.status_code != 200 && e_id_prefix == "activities") {
@@ -54,10 +54,10 @@ async function chaoxingCheckinCaptcha(params, captcha, e_id_prefix) {
 		return;
 	}
 	captcha = res.json()
-	let s = document.getElementById(`${e_id_prefix}-checkin-captcha-input`);
-	let c = document.getElementById(`${e_id_prefix}-checkin-captcha-` +
+	const s = document.getElementById(`${e_id_prefix}-checkin-captcha-input`);
+	const c = document.getElementById(`${e_id_prefix}-checkin-captcha-` +
 					`container-div`);
-	let s_img = document.getElementById(`${e_id_prefix}-checkin-captcha-` +
+	const s_img = document.getElementById(`${e_id_prefix}-checkin-captcha-` +
 					    `small-img`);
 	s.oninput = () => s_img.style.left =
 		     `${(c.offsetWidth - s_img.offsetWidth) * s.value / 320}px`;
@@ -97,7 +97,7 @@ async function chaoxingCheckinCaptcha(params, captcha, e_id_prefix) {
 		});
 		displayTag(`${e_id_prefix}-checkin-captcha-div`);
 	};
-	let img = document.getElementById(`${e_id_prefix}-checkin-captcha-img`);
+	const img = document.getElementById(`${e_id_prefix}-checkin-captcha-img`);
 	img.onload = () => displayTag(`${e_id_prefix}-checkin-captcha-div`);
 	s_img.style.left = `${s.value = 0}px`;
 	s_img.src = captcha.small_img_src;
@@ -107,10 +107,10 @@ async function chaoxingCheckinCaptcha(params, captcha, e_id_prefix) {
 async function chaoxingCheckinLocation(activity) {
 	document.getElementById(`activities-checkin-captcha-div`).style.display
 								       = "none";
-	let res = await post("/chaoxing/checkin_checkin_location", {
+	const res = await post("/chaoxing/checkin_checkin_location", {
 		"location": g_location, "activity": activity
 	});
-	let data = res.json();
+	const data = res.json();
 	if (res.status_code != 200) {
 		alert(`Checkin error. (Backend error, ${res.status_code})`);
 		return;
@@ -128,10 +128,10 @@ async function chaoxingCheckinLocationWrapper(activity) {
 async function chaoxingCheckinQrcode(url, result_div_id) {
 	document.getElementById(`${result_div_id.split('-')[0]}-checkin-` +
 				`captcha-div`).style.display = "none";
-	let res = await post("/chaoxing/checkin_checkin_qrcode_url", {
+	const res = await post("/chaoxing/checkin_checkin_qrcode_url", {
 		"location": g_location, "url": url
 	});
-	let data = res.json();
+	const data = res.json();
 	document.getElementById(result_div_id).innerText =
 						    unescapeUnicode(data.msg) ||
 			   `Checkin error. (Backend error, ${res.status_code})`;
