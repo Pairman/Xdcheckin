9dec0a0ebbf6c5ca32cbf78b4e9522b4e6edaa21
Null <pairmanxlr@gmail.com>
2024-09-21 14:35:29 +0800

    indent

--- src/xdcheckin/__init__.py
+++ src/xdcheckin/__init__.py
@@ -1,7 +1,5 @@
 from xdcheckin.core.chaoxing import Chaoxing
 from xdcheckin.core.xidian import IDSSession, Newesxidian
 from xdcheckin.core.locations import locations
-
 from xdcheckin.server.server import server_routes, create_server, start_server
-
 from xdcheckin.util.captcha import chaoxing_captcha_get_checksum, solve_captcha
