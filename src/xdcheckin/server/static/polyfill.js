838e11907a53b982b8a824803029156188a603d2
Null <pairmanxlr@gmail.com>
2024-09-25 20:41:43 +0800

    load zbarwasm conditionally and use llqrcode as fallback

--- src/xdcheckin/server/static/polyfill.js
+++ src/xdcheckin/server/static/polyfill.js
@@ -1,5 +1,5 @@
 if (typeof globalThis === "undefined")
-	(function () {
+	(function() {
 		if (typeof self !== "undefined")
 			self.globalThis = self;
 		else if (typeof window !== "undefined")
@@ -10,7 +10,7 @@ if (typeof globalThis === "undefined")
 			this.globalThis = this;
 	})();
 
-(function () {
+(function() {
 	function assign (target, ...sources) {
 		if (target == null)
 			throw new TypeError(
@@ -38,8 +38,69 @@ if (typeof globalThis === "undefined")
 })();
 
 if (!Element.prototype.replaceChildren)
-	Element.prototype.replaceChildren = function (...nodes) {
+	Element.prototype.replaceChildren = function(...nodes) {
 		while (this.firstChild)
 			this.firstChild.remove();
 		this.append(...nodes);
 	}
+
+if (typeof WebAssembly === "object") {
+	const script = document.createElement("script");
+	script.src =
+	 "https://cdn.jsdelivr.net/npm/@undecaf/zbar-wasm@0.11.0/dist/index.js";
+	script.onload = async function() {
+		const scanner = await zbarWasm.getDefaultScanner();
+		scanner.setConfig(zbarWasm.ZBarSymbolType.ZBAR_NONE,
+				zbarWasm.ZBarConfigType.ZBAR_CFG_ENABLE, 0);
+		scanner.setConfig(zbarWasm.ZBarSymbolType.ZBAR_QRCODE,
+				zbarWasm.ZBarConfigType.ZBAR_CFG_ENABLE, 1);
+	}
+	document.head.appendChild(script);
+}
+else {
+	globalThis.zbarWasm = {};
+	zbarWasm.getDefaultScanner = function() {
+		return {"setConfig": function() {}}
+	};
+	zbarWasm.ZBarSymbolType = zbarWasm.ZBarConfigType = {};
+	const script = document.createElement("script");
+	globalThis.module = {};
+	script.src = "https://cdn.jsdelivr.net/npm/llqrcode@1.0.0/index.min.js";
+	async function scanImageData (data) {
+		const canvas = document.createElement("canvas");
+		canvas.height = data.height;
+		canvas.width = data.width;
+		const ctx = canvas.getContext("2d");
+		ctx.putImageData(data, 0, 0);
+		let nh = data.height, nw = data.width;
+		if(data.height * data.width > qrcode.maxImgSize) {
+			const r = data.width / data.height;
+			nh = Math.sqrt(qrcode.maxImgSize / r);
+			nw = r * nh;
+		}
+		const ncanvas = document.createElement("canvas");
+		qrcode.height = ncanvas.height = nh;
+		qrcode.width = ncanvas.width = nw;
+		const nctx = ncanvas.getContext("2d");
+		nctx.drawImage(canvas, 0, 0, data.width, data.height, 0, 0, nw,
+			       nh);
+		qrcode.imagedata = nctx.getImageData(0, 0, nw, nh);
+		try {
+			qrcode.result = qrcode.process(nctx);
+			return [{
+				"decode": function() {return qrcode.result;}
+			}];
+		}
+		catch (e) {
+			if (typeof e === "string" &&
+			    e.startsWith("Couldn't find enough"))
+				return [];
+			else
+				throw e;
+		}
+	}
+	script.onload = function() {
+		zbarWasm.scanImageData = scanImageData;
+	}
+	document.head.appendChild(script);
+}
