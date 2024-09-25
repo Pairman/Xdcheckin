51f1538935aeced26549583923017f73271ae59b
Null <pairmanxlr@gmail.com>
2024-09-25 23:14:19 +0800

    Update __init__.py

--- src/xdcheckin/__init__.py
+++ src/xdcheckin/__init__.py
@@ -1,6 +1,7 @@
 from xdcheckin.core.chaoxing import Chaoxing
 from xdcheckin.core.xidian import IDSSession, Newesxidian
 from xdcheckin.core.locations import locations
+from xdcheckin.core.classrooms import classroom_urls, classroom_url_get_single
 from xdcheckin.server.server import server_routes, create_server, start_server
 from xdcheckin.util.captcha import chaoxing_captcha_get_checksum, solve_captcha
 from xdcheckin.util.image import video_get_img, img_scan
