2df9652a5cb50fd81c32be136039bc5a76de253d
Null <pairmanxlr@gmail.com>
2024-09-26 22:19:22 +0800

    rm unused code

--- src/xdcheckin/util/session.py
+++ src/xdcheckin/util/session.py
@@ -1,16 +1,6 @@
 __all__ = ("CachedSession", )
 
-from asyncio import (
-	create_task as _create_task, run as _run,
-	get_event_loop as _get_event_loop
-)
-from atexit import register as _register
-from signal import signal as _signal, SIGINT as _SIGINT, SIGTERM as _SIGTERM
-try:
-	from signal import SIGHUP as _SIGHUP
-except Exception:
-	_SIGHUP = None
-from sys import exit as _exit
+from asyncio import create_task as _create_task
 from aiocache import Cache as _Cache
 from aiocache.serializers import NullSerializer as _NullSerializer
 from aiohttp.client import (
@@ -52,24 +42,6 @@ class CachedSession:
 			self.__cache = _Cache(
 				_Cache.MEMORY, serializer = _NullSerializer()
 			)
-		def _release():
-			try:
-				_loop = _get_event_loop()
-			except Exception:
-				_loop = None
-			_coro = self.__aexit__(None, None, None)
-			if _loop and _loop.is_running():
-				_loop.create_task(_coro)
-			else:
-				_run(_coro)
-		def _sighandler(sig, frame):
-			_release()
-			_exit(0)
-		_register(_release)
-		if _SIGHUP:
-			_signal(_SIGHUP, _sighandler)
-		_signal(_SIGINT, _sighandler)
-		_signal(_SIGTERM, _sighandler)
 
 	async def __aenter__(self):
 		if not self.__async_ctxmgr is None:
